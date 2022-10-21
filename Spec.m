e396aa0304266576b0ff8beffab4d2c80b36d9b3
Jieying-Chen <jieying853@gmail.com>
2022-10-21 09:52:11 +0100

    update file

--- Spec.m
+++ Spec.m
@@ -1,9 +1,9 @@
 clear
 VoicePath = "C:\Users\96326\Desktop\IIBproject\VoiceSource\";
-filename = "Guo.mp3";
+filename = "JLSpeech1.mp3";
 [audio, Fs] = audioread(VoicePath+filename);
 %sound(audio,Fs)
-
+%audio=y_out;
 duration = length(audio)/Fs;
 
 
@@ -15,9 +15,10 @@ overlap = round(0.75*duration / win_time);
 
 [s,f,t] = spectrogram(audio,win_length,overlap,100,Fs,'yaxis');
 %spectrogram(audio,win_length,overlap,1000,Fs,'yaxis')
+%set(gca, 'YDir','reverse')
 
 [s2,f2,t2] = stft(audio,Fs,'Window',win,'OverlapLength',overlap,'FFTLength',500,'FrequencyRange','onesided');
 
 
-
-%iy = istft(s,Fs,'Window',win,'OverlapLength',overlap,'FrequencyRange','onesided');
+s2(50:end,:)=0+0i;
+iy = istft(s2,Fs,'Window',win,'OverlapLength',overlap,'FFTLength',500,'FrequencyRange','onesided');
