1fd2913c77e49b808c4a868051e3841fca876772
Jieying-Chen <jieying853@gmail.com>
2023-02-15 14:38:22 +0000

    save progress

--- utils.py
+++ utils.py
@@ -28,6 +28,30 @@ class Note:
             return 4
 
     
+#Separate notes into C major and Gb major
+def separate_majors(notes):
+    C_major = [1,3,4,6,8,9,11]
+    Gb_major = [0,2,3,5,7,9,10]
+
+    C_notes = []
+    Gb_notes = []
+
+    for i,note in enumerate(notes):
+        seq = note.pitch % 12
+        if (seq in C_major) and (seq not in Gb_major):
+            C_notes.append(notes.pop(i))
+        elif (seq in Gb_major) and (seq not in C_major):
+            Gb_notes.append(notes.pop(i))
+    counter = len(notes)
+    while counter > 0:
+        if counter % 2 == 0:
+            C_notes.append(notes.pop(0))
+        else:
+            Gb_notes.append(notes.pop(0))
+        counter -= 1
+    return [C_notes, Gb_notes]
+
+
 
 ## visualization tool
 def plot_notes(note_list):
@@ -37,3 +61,7 @@ def plot_notes(note_list):
         time.append(note.start)
         pitch.append(note.pitch)
     plt.scatter(time,pitch,s=1)
+
+def print_list(l):
+    for item in l:
+        print(item)
